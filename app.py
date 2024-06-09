71d44c3925dc0612dedbb84fc86c64916df9fb8d
kommineni-venkat <komminenivenkat101@gmail.com>
2024-06-09 18:18:30 +0530

    third commit

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
-# Ensure the UPLOAD_FOLDER exists
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 def fuse_images(image1_path, image2_path):
-    # Load images
+   
     img1 = cv2.imread(image1_path)
     img2 = cv2.imread(image2_path)
 
-    # Resize images to have the same dimensions
+    
     img2_resized = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
 
-    # Perform image fusion (averaging in this case)
+    
     fused_image = cv2.addWeighted(img1, 0.5, img2_resized, 0.5, 0)
 
     return fused_image
@@ -38,21 +38,20 @@ def upload():
     file1 = request.files['file1']
     file2 = request.files['file2']
 
-    # If the user does not select a file, the browser also submits an empty part without a filename
+    
     if file1.filename == '' or file2.filename == '':
         return redirect(request.url)
 
-    # Save the uploaded files
+    
     file1_path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
     file2_path = os.path.join(app.config['UPLOAD_FOLDER'], file2.filename)
 
     file1.save(file1_path)
     file2.save(file2_path)
 
-    # Fuse images
     fused_image = fuse_images(file1_path, file2_path)
 
-    # Save the fused image
+    
     fused_image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'fused_image.jpg')
     cv2.imwrite(fused_image_path, fused_image)
 
